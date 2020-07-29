51a23b72e9d9dee300ddfcfe38bb668007ef24e6
jason-aultman <jason.aultman@gmail.com>
2020-07-28 20:52:33 -0400

    initial commit

---
+++ AHBC_Exercise48withEnumerables/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AHBC_Exercise48withEnumerables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> enumerableList = new List<int>();
+            var nonEnumerableList = (ICollection<int>)enumerableList;
+          
+            string inputAsString;
+
+            do
+            {
+                do
+                {
+                    inputAsString = GetUserInput();
+
+                    if (inputAsString!="q")
+                    {
+                        var inputAsInt=int.Parse(inputAsString);
+                        nonEnumerableList.Add(inputAsInt);
+                    }
+                } while (inputAsString != "q");
+                foreach(int i in enumerableList)
+                {
+                    
+                    if(i!=enumerableList.Last<int>())
+                    Console.Write($"{i}+");
+                    else Console.Write($"{i}");
+                }
+                Console.WriteLine("="+enumerableList.Sum());
+            } while (PromptUserToRunAgain());
+        }
+
+
+        private static string GetUserInput()
+        {
+            Console.WriteLine("Enter a number: q to quit");
+            var userInput = Console.ReadLine();
+            return userInput.ToLower();
+        }
+
+
+        private static bool PromptUserToRunAgain()
+        {
+            Console.WriteLine("Would you like to continue? y/n");
+            var answer = Console.ReadLine();
+            if (answer.Where(thing => answer.StartsWith('y')).Count() > 0)
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Goodbye");
+                return false;
+            }
+        }
+
+       
+        
+    }
+}
